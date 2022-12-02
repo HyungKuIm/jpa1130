1ce5d9c23ed0976bfcbf5e7888d9ac22ff945e76
HyungKu Im <damansa1@naver.com>
2022-12-02 18:17:58 +0900

    put update

--- demojpa2/src/main/java/com/oraclejava/controller/ProductRestController.java
+++ demojpa2/src/main/java/com/oraclejava/controller/ProductRestController.java
@@ -2,6 +2,7 @@ package com.oraclejava.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,6 +46,24 @@ public class ProductRestController {
 	@PostMapping(value="/products", consumes="application/json")
 	public Product postProduct(@RequestBody Product product) {
 		return productRepository.save(product);
+	}
+	
+	@CrossOrigin(origins = "http://localhost:3000")
+	@PutMapping(value="/products", consumes="application/json")
+	public Product putProduct(@RequestBody Product product) {
+		Product foundProduct = productRepository.findById(product.getId()).isPresent()
+					? productRepository.findById(product.getId()).get() : null; 
+		if (foundProduct != null) {
+			foundProduct.setName(product.getName());
+			foundProduct.setBrand(product.getBrand());
+			foundProduct.setType(product.getType());
+			foundProduct.setDescription(product.getDescription());
+			return productRepository.save(foundProduct);
+		} else {
+			return null;
+		}
+		
+		
 	}
 }
 
